0728a711eaa9a83280491ad62c0be8ea51989e97
Piyush Kumar Singh <2024ce114f@sigce.edu.in>
2026-04-10 22:56:58 +0530

    removed use effect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -11,19 +12,35 @@ import EventPlanDashboard from "./pages/EventPlanDashboard"
 import AIDashboard from "./pages/AIDashboard"
 
 function App() {
+  
+const isAuth = localStorage.getItem("token");
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<h1>Route Not Found</h1>} />
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create-event" element={<CreateEvent />} />
-        <Route path="/event/:id" element={<EventDetails />} />
-        <Route path="/event/:id/volunteers" element={<VolunteerDashboard />} />
-        <Route path="/event/:id/budget" element={<BudgetDashboard />} />
-        <Route path="/event/:id/plan" element={<EventPlanDashboard />} />
-        <Route path="/event/:id/ai" element={<AIDashboard />} />
+        <Route path="/dashboard" element={
+            isAuth ? <Dashboard /> : <Navigate to="/login" />
+          } />
+        <Route path="/create-event" element={
+            isAuth ? <CreateEvent /> : <Navigate to="/login" />
+          } />
+        <Route path="/event/:id" element={
+            isAuth ? <EventDetails /> : <Navigate to="/login" />
+          } />
+        <Route path="/event/:id/volunteers" element={
+            isAuth ? <VolunteerDashboard /> : <Navigate to="/login" />
+          } />
+        <Route path="/event/:id/budget" element={
+            isAuth ? <BudgetDashboard /> : <Navigate to="/login" />
+          } />
+        <Route path="/event/:id/plan" element={
+            isAuth ? <EventPlanDashboard /> : <Navigate to="/login" />
+          } />
+        <Route path="/event/:id/ai" element={
+            isAuth ? <AIDashboard /> : <Navigate to="/login" />
+          } />
       </Routes>
     </BrowserRouter>
   );
