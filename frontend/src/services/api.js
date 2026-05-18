b87bd7a484568a39d7ad0d38f13ccd3972cae90e
Piyush Kumar Singh <2024ce114f@sigce.edu.in>
2026-05-18 18:41:21 +0530

    editied api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,23 @@
 import axios from "axios";
 
 const API = axios.create({
+
   // baseURL: "https://intelliventbackend.vercel.app/api", // For Global deployment
   baseURL:"https://intellivent-git-features-piyush112007s-projects.vercel.app/api", // For Feature deployment
   // baseURL: "http://localhost:5000/api", //For Local Testing
-  // withCredentials: true,
+  
+  withCredentials: true,
+
+});
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
 });
 
 export default API;
