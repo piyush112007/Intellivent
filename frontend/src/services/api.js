bfa28b64beed5ab6cd2af2b96b5b87983cb1cc84
Piyush Kumar Singh <2024ce114f@sigce.edu.in>
2026-05-18 17:08:41 +0530

    added feature deplolyment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://intelliventbackend.vercel.app/api", // For Global deployment
+  // baseURL: "https://intelliventbackend.vercel.app/api", // For Global deployment
+  baseURL:
+    "https://intellivent-git-features-piyush112007s-projects.vercel.app/", // For Feature deployment
   // baseURL: "http://localhost:5000/api", //For Local Testing
   withCredentials: true,
 });
