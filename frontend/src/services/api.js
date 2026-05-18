d27686d5c8f157f3b5aac1b4a5f3ebd762ddabe2
Piyush Kumar Singh <2024ce114f@sigce.edu.in>
2026-05-18 17:51:28 +0530

    s

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
   // baseURL: "https://intelliventbackend.vercel.app/api", // For Global deployment
   baseURL:"https://intellivent-git-features-piyush112007s-projects.vercel.app/api", // For Feature deployment
   // baseURL: "http://localhost:5000/api", //For Local Testing
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default API;
